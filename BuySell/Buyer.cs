c242613e54a20730d3cb0f4a9168e1f38fbfff04
Соценко Владислав <vladyslav.sotsenko@nure.ua>
2026-05-12 23:56:08 +0200

    Створено класи покупця та продавця

---
+++ BuySell/Buyer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuySell
+{
+    internal class Buyer
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal MaxPrice { get; set; }
+        public string PaymentType { get; set; }
+        public string Contact { get; set; }
+        public string Note { get; set; }
+    }
+}
---
+++ BuySell/Seller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuySell
+{
+    internal class Seller
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string DeliveryTerms { get; set; }
+        public string PaymentType { get; set; }
+        public string Contact { get; set; }
+        public string Note { get; set; }
+    }
+}
